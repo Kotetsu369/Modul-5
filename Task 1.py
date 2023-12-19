ef0408e01a218c300f66e3698d2bd0875899fefe
Kotetsu369 <christi-95@mail.ru>
2023-12-19 22:37:22 +0900

    task 1

---
+++ Task 1.py
@@ -0,0 +1,68 @@
+import time
+
+start_time = time.time()
+def get_thread(thread_name):
+    time.sleep(1)
+    print(f'my name {thread_name}')
+    pass
+
+get_thread('one')
+get_thread('two')
+get_thread('three')
+get_thread('four')
+get_thread('five')
+
+end_time = time.time()  # время окончания выполнения
+execution_time = end_time - start_time  # вычисляем время выполнения
+
+print(f"Время выполнения программы: {execution_time} секунд")
+
+import threading
+
+
+start_time = time.time()
+import asyncio
+
+async def get_thread_one():
+    await asyncio.sleep(1)
+    print(f'my name one')
+    pass
+
+async def get_thread_two():
+    await asyncio.sleep(1)
+    print(f'my name two')
+    pass
+
+async def get_thread_three():
+    await asyncio.sleep(1)
+    print(f'my name three')
+
+async def get_thread_four():
+    await asyncio.sleep(1)
+    print(f'my name four')
+    pass
+
+async def get_thread_five():
+    await asyncio.sleep(1)
+    print(f'my name five')
+    pass
+
+async  def main():
+    t1 = asyncio.create_task(get_thread_one())
+    t2 = asyncio.create_task(get_thread_two())
+    t3 = asyncio.create_task(get_thread_three())
+    t4 = asyncio.create_task(get_thread_four())
+    t5 = asyncio.create_task(get_thread_five())
+    await t1
+    await t2
+    await t3
+    await t4
+    await t5
+
+asyncio.run(main())
+end_time = time.time()  # время окончания выполнения
+execution_time = end_time - start_time  # вычисляем время выполнения
+
+print(f"Время выполнения программы: {execution_time} секунд")
+
+
